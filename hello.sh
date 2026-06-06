38e502fb2421e3adf10cafe3bf98db1aa5173ce5
Valens <valens@example.com>
2026-06-06 18:24:22 +0200

    second commit

--- hello.sh
+++ hello.sh
@@ -1,2 +1,3 @@
 echo "Hello, World!"
 echo "This is a simple Jenkins pipeline example."
+echo "Jenkins is a powerful automation server that can help you automate your software development processes."@
